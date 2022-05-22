0fe8b0800c411b03b295c798d8d874df02c62bf4
Felipe Gochomoto <felipe_gocho@hotmail.com>
2022-05-22 00:18:07 -0300

    Initial commit

---
+++ src/main/java/br/com/projetofinal/login/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.com.projetofinal.login.repository;
+
+import br.com.projetofinal.login.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
